d7ff5c9f3f90843fec63e301c449edcd5b07e679
Vadzim11 <vadzim11@tut.by>
2017-04-16 21:06:12 +0200

    add spring secure auth with encrypt

--- src/main/java/com/springProject/config/AppContext.java
+++ src/main/java/com/springProject/config/AppContext.java
@@ -9,37 +9,52 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 
 @Configuration
 @PropertySource(value = {"classpath:util.properties"})
-public class AppContext{
+@PropertySource(value = {"classpath:auth.properties"})
+public class AppContext {
     @Autowired
     Environment environment;
+
+    @Autowired
+    HibernateConfig hibernateConfig;
 
     @Bean
     public Message message() {
         return new Message("Hello world");
-}
+    }
 
     @Bean
-     public JdbcTemplate jdbcTemplate() {
+    public JdbcTemplate jdbcTemplate() {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
         jdbcTemplate.setDataSource(dataSource());
         return jdbcTemplate;
-     }
+    }
 
-     @Bean
-      public CreateTable createTable(){
+    @Bean
+    public UserDetailsService userDetailsService() {
+        JdbcDaoImpl jdbcImpl = new JdbcDaoImpl();
+        jdbcImpl.setDataSource(hibernateConfig.dataSource());
+        jdbcImpl.setUsersByUsernameQuery(environment.getRequiredProperty("usersByQuery"));
+        jdbcImpl.setAuthoritiesByUsernameQuery(environment.getRequiredProperty("rolesByQuery"));
+        return jdbcImpl;
+    }
+
+    @Bean
+    public CreateTable createTable() {
         return new CreateTable(jdbcTemplate());
-      }
+    }
 
-      @Bean
-      public DriverManagerDataSource dataSource() {
+    @Bean
+    public DriverManagerDataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(environment.getProperty("jdbc.postgresql.driverClass"));
         dataSource.setUrl(environment.getProperty("jdbc.postgresql.url"));
         dataSource.setUsername(environment.getProperty("jdbc.postgresql.username"));
         dataSource.setPassword(environment.getProperty("jdbc.postgresql.password"));
         return dataSource;
-      }
+    }
 }
